4dbfe333d9e183e1fbcc8fe0b33e0cc1d6c5c58c
Elijah O Ajibade <elijahbade360@gmail.com>
2024-10-28 09:34:33 +0100

    deleteNote function updated for error handling, getNotebyID route implemented

--- src/contollers/noteControllers.ts
+++ src/contollers/noteControllers.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import Note, { INote } from '../models/Note';
 
 // Get all notes
@@ -43,12 +43,41 @@ export const updateNote = async (req: Request, res: Response) => {
 
 // Delete a note
 export const deleteNote = async (req: Request, res: Response) => {
-  const { id } = req.params;
+
 
   try {
-    await Note.findByIdAndDelete(id);
-    res.json({ message: 'Note deleted' });
+
+    const { id } = req.params;
+    const note = await Note.findById(id);
+
+    if (!note) {
+      res.status(404).json({message: "Note not found!"})
+    }
+
+    await Note.deleteOne({_id: id});
+    res.status(200).json({messag: "Note has been successfuly deleted"})
+    
+ 
   } catch (error) {
-    res.status(500).json({ message: 'Failed to delete note' });
+    res.status(500).json({ message: 'server error' });
+    console.log(error);
+  }
+};
+
+
+
+
+// Get a single note by ID
+export const getNoteById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const noteId = req.params.id;
+    const note = await Note.findById(noteId);
+    if (!note) {
+      res.status(404).json({ message: 'Note not found' });
+      return; 
+    }
+    res.json(note);
+  } catch (error) {
+    next(error);
   }
 };
--- src/routes/noteRoutes.ts
+++ src/routes/noteRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getNotes, createNote, updateNote, deleteNote } from '../contollers/noteControllers';
+import { getNotes, createNote, updateNote, deleteNote, getNoteById } from '../contollers/noteControllers';
 
 const router = Router();
 
 router.get('/notes', getNotes);
+router.get('/notes/:id', getNoteById);
 router.post('/notes', createNote);
 router.put('/notes/:id', updateNote);
 router.delete('/notes/:id', deleteNote);
